310d82f83740bd245b43f998e30ab228aec5e20c
danikanjw <danikanajwaa@gmail.com>
2024-05-28 20:42:06 +0700

    Perekam Waktu Kerja 1.0

---
+++ src/main/java/com/mycompany/productivitytracker/ProductivityTracker.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.productivitytracker;
+
+/**
+ *
+ * @author oni permatasari
+ */
+public class ProductivityTracker {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
