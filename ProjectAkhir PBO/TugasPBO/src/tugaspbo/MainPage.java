2916925ec872b2060620690b5ef1e5892cae9e8c
Ferry Hasan <ferryhasanbackup@gmail.com>
2024-05-30 10:10:29 +0700

    style: add Update button in MainPage

--- ProjectAkhir PBO/TugasPBO/src/tugaspbo/MainPage.java
+++ ProjectAkhir PBO/TugasPBO/src/tugaspbo/MainPage.java
@@ -61,6 +61,7 @@ public class MainPage extends javax.swing.JFrame {
         stopTime = new javax.swing.JFormattedTextField();
         startTime = new javax.swing.JFormattedTextField();
         timeLabel = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -120,6 +121,14 @@ public class MainPage extends javax.swing.JFrame {
 
         timeLabel.setText("hh : mm a");
 
+        jButton1.setBackground(new java.awt.Color(255, 255, 0));
+        jButton1.setText("Update");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -154,6 +163,10 @@ public class MainPage extends javax.swing.JFrame {
                                 .addGap(33, 33, 33)
                                 .addComponent(goBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)))
                         .addContainerGap(495, Short.MAX_VALUE))))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(335, 335, 335))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -173,7 +186,9 @@ public class MainPage extends javax.swing.JFrame {
                 .addComponent(prodStart)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(prodStop)
-                .addGap(42, 42, 42)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton1)
+                .addGap(13, 13, 13)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(stopLabel)
                     .addComponent(stopTime, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -286,6 +301,10 @@ public class MainPage extends javax.swing.JFrame {
     private void startTimeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_startTimeActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_startTimeActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
     
     
     /**
@@ -326,6 +345,7 @@ public class MainPage extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel doneByLabel;
     private javax.swing.JButton goBtn;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel prodLabel;
     private javax.swing.JButton prodStart;
     private javax.swing.JButton prodStop;
