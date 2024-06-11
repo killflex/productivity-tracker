3fd5bf2c3f76fa6359459bf947fa5cba49423520
NathanaelGT <gordontanu2004@gmail.com>
2024-06-11 17:50:18 +0700

    chore: Membuat class Pengguna dan Catatan

---
+++ src/tugaspbo/Catatan.java
@@ -0,0 +1,195 @@
+package tugaspbo;
+
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+public class Catatan {
+    private int id;
+    private LocalTime targetMulai;
+    private LocalTime targetSelesai;
+    private LocalTime mulai;
+    private LocalTime selesai;
+    private LocalTime targetProduktifitas;
+    private double tingkatKeproduktifan;
+    private LocalDate tanggal;
+
+    public Catatan(
+            int id,
+            LocalTime targetMulai,
+            LocalTime targetSelesai,
+            LocalTime mulai,
+            LocalTime selesai,
+            LocalTime targetProduktifitas,
+            double tingkatKeproduktifan,
+            LocalDate tanggal
+    ) {
+        this.id = id;
+        this.targetMulai = targetMulai;
+        this.targetSelesai = targetSelesai;
+        this.mulai = mulai;
+        this.selesai = selesai;
+        this.targetProduktifitas = targetProduktifitas;
+        this.tingkatKeproduktifan = tingkatKeproduktifan;
+        this.tanggal = tanggal;
+    }
+
+    protected static Catatan fromResultSet(ResultSet result) {
+        try {
+            return new Catatan(
+                    result.getInt("id"),
+                    result.getTime("target_mulai").toLocalTime(),
+                    result.getTime("target_selesai").toLocalTime(),
+                    result.getTime("mulai").toLocalTime(),
+                    result.getTime("selesai").toLocalTime(),
+                    result.getTime("target_produktifitas").toLocalTime(),
+                    result.getDouble("tingkat_keproduktifan"),
+                    result.getDate("tanggal").toLocalDate()
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Catatan baru(
+            Pengguna pengguna,
+            LocalTime targetMulai,
+            LocalTime targetSelesai,
+            LocalTime mulai,
+            LocalTime selesai,
+            LocalTime targetProduktifitas,
+            double tingkatKeproduktifan,
+            LocalDate tanggal
+    ) {
+        Database.execute(
+                "INSERT INTO catatan (id_pengguna, target_mulai, target_selesai, mulai, selesai, target_produktifitas, tingkat_keproduktifan, tanggal) VALUES (" +
+                pengguna.getId() + ", " +
+                "'" + targetMulai + "', " +
+                "'" + targetSelesai + "', " +
+                "'" + mulai + "', " +
+                "'" + selesai + "', " +
+                "'" + targetProduktifitas + "', " +
+                (tingkatKeproduktifan == 0 ? "null" : tingkatKeproduktifan) + ", " +
+                "'" + Date.valueOf(tanggal) + "')"
+        );
+
+        return cari(pengguna, tanggal);
+    }
+
+    public static ArrayList<Catatan> cari(Pengguna pengguna) {
+        ResultSet resultSet = Database.query("SELECT * FROM catatan WHERE id_pengguna = " + pengguna.getId());
+        ArrayList<Catatan> catatan = new ArrayList<>();
+
+        try {
+            while (resultSet.next()) {
+                catatan.add(fromResultSet(resultSet));
+            }
+        } catch (SQLException e) {
+            //
+        }
+
+        return catatan;
+    }
+
+    public static Catatan cari(Pengguna pengguna, LocalDate tanggal) {
+        String tgl = tanggal.getYear() + "-" + tanggal.getMonthValue() + "-" + tanggal.getDayOfMonth();
+
+        ResultSet resultSet = Database.query(
+                "SELECT * FROM catatan WHERE id_pengguna = " + pengguna.getId() + " and tanggal = '" + tgl + "'"
+        );
+
+        try {
+            if (resultSet.next()) {
+                return fromResultSet(resultSet);
+            }
+        } catch (Exception e) {
+            //
+        }
+
+        return null;
+    }
+
+    public void update() {
+        Database.execute(
+                "UPDATE catatan SET " +
+                "target_mulai = '" + targetMulai + "', " +
+                "target_selesai = '" + targetSelesai + "', " +
+                "mulai = '" + mulai + "', " +
+                "selesai = '" + selesai + "', " +
+                "target_produktifitas = '" + targetProduktifitas + "', " +
+                "tingkat_keproduktifan = '" + (tingkatKeproduktifan == 0 ? "null" : tingkatKeproduktifan) + "'" +
+                "WHERE id = " + id
+        );
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public LocalTime getTargetMulai() {
+        return targetMulai;
+    }
+
+    public Catatan setTargetMulai(LocalTime targetMulai) {
+        this.targetMulai = targetMulai;
+
+        return this;
+    }
+
+    public LocalTime getTargetSelesai() {
+        return targetSelesai;
+    }
+
+    public Catatan setTargetSelesai(LocalTime targetSelesai) {
+        this.targetSelesai = targetSelesai;
+
+        return this;
+    }
+
+    public LocalTime getMulai() {
+        return mulai;
+    }
+
+    public Catatan setMulai(LocalTime mulai) {
+        this.mulai = mulai;
+
+        return this;
+    }
+
+    public LocalTime getSelesai() {
+        return selesai;
+    }
+
+    public Catatan setSelesai(LocalTime selesai) {
+        this.selesai = selesai;
+
+        return this;
+    }
+
+    public LocalTime getTargetProduktifitas() {
+        return targetProduktifitas;
+    }
+
+    public Catatan setTargetProduktifitas(LocalTime targetProduktifitas) {
+        this.targetProduktifitas = targetProduktifitas;
+
+        return this;
+    }
+
+    public double getTingkatKeproduktifan() {
+        return tingkatKeproduktifan;
+    }
+
+    public Catatan setTingkatKeproduktifan(double tingkatKeproduktifan) {
+        this.tingkatKeproduktifan = tingkatKeproduktifan;
+
+        return this;
+    }
+
+    public LocalDate getTanggal() {
+        return tanggal;
+    }
+}
---
+++ src/tugaspbo/Pengguna.java
@@ -0,0 +1,158 @@
+package tugaspbo;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+public class Pengguna {
+    private int id;
+    private String nama;
+    private String password;
+    private LocalTime targetMulai;
+    private LocalTime targetSelesai;
+    private LocalTime targetProduktifitas;
+    private ArrayList<Catatan> catatan = null;
+    private Catatan catatanHariIni = null;
+
+    public Pengguna(
+            int id,
+            String nama,
+            String password,
+            LocalTime targetMulai,
+            LocalTime targetSelesai,
+            LocalTime targetProduktifitas
+    ) {
+        this.id = id;
+        this.nama = nama;
+        this.password = password;
+        this.targetMulai = targetMulai;
+        this.targetSelesai = targetSelesai;
+        this.targetProduktifitas = targetProduktifitas;
+    }
+
+    protected static Pengguna fromResultSet(ResultSet result) {
+        try {
+            return new Pengguna(
+                    result.getInt("id"),
+                    result.getString("nama"),
+                    result.getString("password"),
+                    result.getTime("target_mulai").toLocalTime(),
+                    result.getTime("target_selesai").toLocalTime(),
+                    result.getTime("target_produktifitas").toLocalTime()
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Pengguna baru(String nama, String password) {
+        Database.execute(
+                "INSERT INTO pengguna (nama, password, target_mulai, target_selesai, target_produktifitas) VALUES (" +
+                        "'" + nama + "', " +
+                        "'" + password + "', " +
+                        "'00:00:00', " +
+                        "'00:00:00', " +
+                        "'00:00:00')"
+        );
+
+        return cari(nama);
+    }
+
+    public static Pengguna cari(String nama) {
+        ResultSet resultSet = Database.query("SELECT * FROM pengguna WHERE nama = '" + nama + "'");
+
+        try {
+            if (resultSet.next()) {
+                return fromResultSet(resultSet);
+            }
+        } catch (Exception e) {
+            //
+        }
+
+        return null;
+    }
+
+    public ArrayList<Catatan> catatan() {
+        if (catatan == null) {
+            catatan = Catatan.cari(this);
+        }
+
+        return catatan;
+    }
+
+    public Catatan catatanHariIni() {
+        if (catatanHariIni == null) {
+            catatanHariIni = Catatan.cari(this, LocalDate.now());
+        }
+
+        return catatanHariIni;
+    }
+
+    public void updateTargetMulai(int jam, int menit) {
+        Database.execute("UPDATE pengguna SET target_mulai = '" + jam + ":" + menit + ":00' WHERE id = " + id);
+    }
+
+    public void updateTargetSelesai(int jam, int menit) {
+        Database.execute("UPDATE pengguna SET target_selesai = '" + jam + ":" + menit + ":00' WHERE id = " + id);
+    }
+
+    public void updateTargetProduktifitas(int jam, int menit) {
+        Database.execute("UPDATE pengguna SET target_produktifitas = '" + jam + ":" + menit + ":00' WHERE id = " + id);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public Pengguna setNama(String nama) {
+        this.nama = nama;
+
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Pengguna setPassword(String password) {
+        this.password = password;
+
+        return this;
+    }
+
+    public LocalTime getTargetMulai() {
+        return targetMulai;
+    }
+
+    public Pengguna setTargetMulai(LocalTime targetMulai) {
+        this.targetMulai = targetMulai;
+
+        return this;
+    }
+
+    public LocalTime getTargetSelesai() {
+        return targetSelesai;
+    }
+
+    public Pengguna setTargetSelesai(LocalTime targetSelesai) {
+        this.targetSelesai = targetSelesai;
+
+        return this;
+    }
+
+    public LocalTime getTargetProduktifitas() {
+        return targetProduktifitas;
+    }
+
+    public Pengguna setTargetProduktifitas(LocalTime targetProduktifitas) {
+        this.targetProduktifitas = targetProduktifitas;
+
+        return this;
+    }
+}
